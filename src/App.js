f2d46b1b88872a01d3423aba3556962521e482ba
Xinan <youkingan11@gmail.com>
2024-07-04 13:30:44 +0100

    Added ChatGPT API to achieve chatbot

--- src/App.js
+++ src/App.js
@@ -1,10 +1,139 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
+import {
+  MainContainer,
+  ChatContainer,
+  MessageList,
+  Message,
+  MessageInput,
+  TypingIndicator,
+} from "@chatscope/chat-ui-kit-react";
+import OpenAI from "openai";
+
+const API_KEY = process.env.REACT_APP_OPENAI_API_KEY;
+// "Explain things like you would to a 10 year old learning how to code."
+const systemMessage = {
+  //  Explain things like you're talking to a software professional with 5 years of experience.
+  role: "system",
+  content:
+    "Explain things like you're talking to a software professional with 2 years of experience.",
+};
+
+const openai = new OpenAI({
+  apiKey: API_KEY,
+  dangerouslyAllowBrowser: true, // Enable this option for testing purposes
+});
 
 function App() {
+  const [messages, setMessages] = useState([
+    {
+      message: "Hello, I'm ChatGPT! Ask me anything!",
+      sentTime: "just now",
+      sender: "ChatGPT",
+    },
+  ]);
+  const [isTyping, setIsTyping] = useState(false);
+
+  const handleSend = async (message) => {
+    const newMessage = {
+      message,
+      direction: "outgoing",
+      sender: "user",
+    };
+
+    const newMessages = [...messages, newMessage];
+
+    setMessages(newMessages);
+
+    // Initial system message to determine ChatGPT functionality
+    // How it responds, how it talks, etc.
+    setIsTyping(true);
+    await processMessageToChatGPT(newMessages);
+  };
+
+  async function processMessageToChatGPT(chatMessages) {
+    // messages is an array of messages
+    // Format messages for chatGPT API
+    // API is expecting objects in format of { role: "user" or "assistant", "content": "message here"}
+    // So we need to reformat
+
+    let apiMessages = chatMessages.map((messageObject) => {
+      let role = "";
+      if (messageObject.sender === "ChatGPT") {
+        role = "assistant";
+      } else {
+        role = "user";
+      }
+      return { role: role, content: messageObject.message };
+    });
+
+    // Get the request body set up with the model we plan to use
+    // and the messages which we formatted above. We add a system message in the front to'
+    // determine how we want chatGPT to act.
+    const apiRequestBody = {
+      //"model": "gpt-3.5-turbo",
+      //"model": "gpt-4-turbo-2024-04-09",
+      //"model": "gpt-4o",
+      model: "gpt-3.5-turbo-1106",
+      messages: [
+        systemMessage, // The system message DEFINES the logic of our chatGPT
+        ...apiMessages, // The messages from our chat with ChatGPT
+      ],
+    };
+
+    try {
+      const response = await openai.chat.completions.create(apiRequestBody);
+
+      if (!response || !response.choices || response.choices.length === 0) {
+        throw new Error("No response from API");
+      }
+
+      setMessages([
+        ...chatMessages,
+        {
+          message: response.choices[0].message.content,
+          sender: "ChatGPT",
+        },
+      ]);
+    } catch (error) {
+      console.error("Error during API call:", error.message);
+      console.error(error.response ? error.response.data : error);
+
+      setMessages([
+        ...chatMessages,
+        {
+          message: "Sorry, there was an error. Please try again.",
+          sender: "ChatGPT",
+        },
+      ]);
+    } finally {
+      setIsTyping(false);
+    }
+  }
+
   return (
     <div className="App">
-      <h1>Hello World, This is my React App</h1>
+      <div style={{ position: "relative", height: "800px", width: "700px" }}>
+        <MainContainer>
+          <ChatContainer>
+            <MessageList
+              scrollBehavior="smooth"
+              typingIndicator={
+                isTyping ? (
+                  <TypingIndicator content="ChatGPT is typing" />
+                ) : null
+              }
+            >
+              {messages.map((message, i) => {
+                console.log(message);
+                return <Message key={i} model={message} />;
+              })}
+            </MessageList>
+            <MessageInput placeholder="Type message here" onSend={handleSend} />
+          </ChatContainer>
+        </MainContainer>
+      </div>
     </div>
   );
 }
